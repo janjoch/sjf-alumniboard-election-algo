6ca5c36dce9ad8be4c54a4d614174dcb22ded600
Janjo <janjo@duck.com>
2024-02-09 01:43:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
-# sjf-alumniboard-election-algo
+# SJf Evaluation Wahl Alumni-Rat
 
-Warning: under construction
 
-The automatic and transparent algorithm to select the candidates which fulfil all diversity criteria.
+
+*Algorithmus zur eindeutigen Wahlevaluation für den SJf-Alumni-Rat anhand der bindenden Diversitätskriterien*
+
+(Art. 4 Organisationsreglement des SJf-Alumni-Rates, Version 30.11.2023)
+
+
+#### Ausführen
+
+Online auführen auf Binder:
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/janjoch/sjf-alumniboard-election-algo/HEAD?filepath=EVALUATION.ipynb)
+
+
+#### Kriterien
+
+* `A1` Mindestens zwei (2) Personen aus der Deutschschweiz;
+* `A2` mindestens eine (1) Person aus der französischen Schweiz;
+* `A3` mindestens eine (1) Person aus der italienischen Schweiz. Massgebend ist die Muttersprache. 
+* `B` Mindestens drei (3) Personen sind zum Zeitpunkt des Amtsantrittes Alumni bzw. Alumnae durch erstmalige Teilnahme am Nationalen Wettbewerb oder ISTF der letzten drei (3) Kalenderjahren. 
+* `C` Mindestens eine (1) Person hat vor mehr als zehn (10) Jahren erstmals an einem Wettbewerb oder ISTF teilgenommen.
+* `D` Maximal zwei (2) Personen sind nicht Alumni bzw. Alumnae von SJf. 
+
+
+#### Rohdaten
+
+Die Ergebnisse der Wahl inkl. Informationen über die erfüllten Diversitätskriterien der Kandidat\*innen sind in der Datei `Wahlergebnisse.docx` im vorgegebenen Format abzulegen. Die hier vorhandene Beispieldatei kann ersetzt werden, oder aber der Dateipfad im Programm angepasst werden.
+
+Kandidat\*innen mit der selben Anzahl Stimmen sollten beim Erfassen per Los sortiert werden. Der Algorithmus evaluiert bei Stimmengleichheit die Einträge von oben nach unten. (Aus Gründen der Reproduzierbarkeit wurde auf zufällige Auslosung im Algorithmus verzichtet.)
+
+
+#### Algorithmus
+
+1. Die Kandidat\*innen werden in einer Rangliste nach Anzahl Stimmen sortiert.
+2. Die Kombinationen werden systematisch auf die Erfüllung aller Kriterien geprüft:
+
+    1. Ränge 1-7
+    2. Ränge 1-6+8
+    3. Ränge 1-5+7-8
+    4. ...
+    5. Ränge 2-8
+    6. Ränge 1-6+9
+    7. Ränge 1-5+8-9
+    8. Ränge 1-4+7-9
+    9. ...
+
+
+#### Implementierung des Algorithmus
+
+Der Quellcode ist in der Datei `election_algorithm.py` zu finden. Das Programm wurde ausgiebig getestet, es wird jedoch keine Haftung übernommen. Die generierten Resultate sollten auf Plausibilität geprüft werden!
+
+Autor: Janosch Jörg (Arbeitsgruppe Alumni-Rat), 2024, [github.com/janjoch](https://github.com/janjoch), [janjo@duck.com](mailto:janjo@duck.com)
+
--- election_algorithm.py
+++ election_algorithm.py
@@ -3,12 +3,189 @@ import numpy as np
 import pandas as pd
 
 
-def import_xlsx(
-    io,
-):
-    df = pd.read_excel(
-        io=io,
-        skiprows=(1,),
-        false_values=(np.nan, "", pd.NA),
-    )
-    return df
+def pd_styling_color_boolean(val):
+    color =''
+    if val == True:
+        color = 'white'
+    elif val == False:
+        color = 'lightgrey'
+    return 'background-color: %s' % color
+
+
+def pd_rewrite_bool(val):
+    if val == True:
+        return "1"
+    elif val == False:
+        return "0"
+    return val
+
+
+def pd_styling(styler):
+    for col in ("votes", "total votes"):
+        if(col in styler.data.columns):
+            styler.background_gradient(axis=None, cmap="YlGnBu", subset=col)
+    styler.format(pd_rewrite_bool)
+    styler.applymap(pd_styling_color_boolean)
+    styler.set_table_styles([  # create internal CSS classes
+        {'selector': '.elected', 'props': 'background-color: green;'},
+    ], overwrite=False)
+    return styler
+
+
+class ElectionEvaluation:
+
+    # Compliance rules according to Art. 4 of "Organisationsreglement des SJf-Alumni-Rates" version 30.11.2023
+    COMPLIANCE_RULES = {
+        "A1": lambda x: x >= 2,
+        "A2": lambda x: x >= 1,
+        "A3": lambda x: x >= 1,
+        "B": lambda x: x >= 3,
+        "C": lambda x: x >= 1,
+        "D": lambda x: ~x <= 2,
+    }
+    N_MEMBERS = 7
+
+    def __init__(
+        self,
+        file_path,
+        verbose=True,
+    ):
+        self.verbose = verbose
+        self.results = self.import_xlsx(file_path)
+        self.find_compliant_combination()
+
+    @property
+    def data(
+        self,
+    ):
+        return self.results
+    
+    @property
+    def data_sorted(
+        self,
+    ):
+        return self.get_sorted()
+    
+    def _repr_html_(self):
+        return self.data_sorted.style.pipe(pd_styling)._repr_html_()
+
+    def import_xlsx(
+        self,
+        io,
+        **kwargs,
+    ):
+        return pd.read_excel(
+            io=io,
+            skiprows=(1,),
+            false_values=(np.nan, "", pd.NA),
+            converters={
+                "A1": np.bool,
+                "A2": np.bool,
+                "A3": np.bool,
+                "B": np.bool,
+                "C": np.bool,
+                "D": np.bool,
+            },
+            **kwargs,
+        )
+
+
+    def get_sorted(self, by="votes", ascending=False):
+        return self.results.sort_values(by=by, ascending=ascending)
+
+    def subset_results(self, selection):
+        return self.results.iloc[list(selection)]
+    
+    def check_compliance(self, selection):
+        subset = self.subset_results(selection)
+        counts = subset.sum()
+        if self.verbose:
+            print(counts)
+        compliance = {
+                key: check(counts[key])
+                for (key, check)
+                in self.COMPLIANCE_RULES.items()
+        }
+
+        return compliance
+
+    def find_compliant_combination(self, n_members=None):
+        self._report = pd.DataFrame(columns=("candidates", "ranks", "total votes") + tuple(self.COMPLIANCE_RULES.keys()) + ("compliant",))
+        index_sorted = self.get_sorted().index
+        gen = self.valid_indexes_bool(n_members=n_members)
+
+        while True:
+
+            # bool array for candidate selection
+            selection_bool = next(gen)
+
+            if(len(selection_bool) > len(index_sorted)):
+                print("WARNING: NO COMPLIANT COMBINATION FOUND")
+                return
+
+            selection_bool_extended = np.append(selection_bool, [False] * (len(index_sorted) - len(selection_bool)))
+            index_selection = index_sorted[selection_bool_extended]
+
+            compliance = self.check_compliance(index_selection)
+
+            compliant = np.array(list(compliance.values())).all()
+
+            self.add_report_row(index_selection, selection_bool_extended, compliance, compliant)
+
+            if self.verbose:
+                print(index_selection)
+                print(self.results["candidate_name"][index_selection])
+                print(compliance)
+
+            # COMPLIANT COMBINATION FOUND
+            if compliant:
+                self.results["elected"] = False
+                self.results.loc[index_selection, "elected"] = True
+                return index_selection
+
+    def add_report_row(self, index_selection, selection_bool, compliance, compliant):
+        self._report = self._report.append(
+            pd.Series(
+                {
+                    "candidates": ", ".join(self.results["candidate_name"][index_selection]),
+                    "ranks": ",".join(map(str, np.where(selection_bool)[0])),
+                    "total votes": self.results["votes"][index_selection].sum(),
+                    **compliance,
+                    "compliant": compliant,
+                }
+            ),
+            ignore_index=True,
+        )
+
+    @property
+    def report(self):
+        return self._report.style.pipe(pd_styling)
+
+    def int_to_bool_array(self, number):
+        return np.flip(
+            np.array(
+                [
+                    bit=="1"
+                    for bit
+                    in list(bin(number)[2:])
+                ]
+            )
+        )
+
+    def valid_indexes_int(self, n_members=None):
+        if n_members is None:
+            n_members = self.N_MEMBERS
+        i = 0
+        while True:
+            if(self.int_to_bool_array(i).sum() == n_members):
+                yield i
+            i += 1
+
+
+    def valid_indexes_bool(self, n_members=None):
+        gen = self.valid_indexes_int(n_members=n_members)
+        while True:
+            yield self.int_to_bool_array(next(gen))
+
+
+## no rubber ducks were harmed during debugging ##
